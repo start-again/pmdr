7000e2d95013eb1845412c7b548f62973f673126
LucasCtrl <lucasalt@protonmail.com>
2020-03-22 13:47:22 +0100

    Patch #2 - Notification actions

--- static/custom-sw.js
+++ static/custom-sw.js
@@ -5,7 +5,8 @@ self.addEventListener('notificationclick', function(event) {
   // Close the notification when she's clicked
   event.notification.close()
 
-  // This looks to see if the current window is already open and focuses if it is
+  // This looks to see if the current window is already open and
+  // focuses if it is
   event.waitUntil(
     clients
       .matchAll({
@@ -14,7 +15,12 @@ self.addEventListener('notificationclick', function(event) {
       .then(function(clientList) {
         for (let i = 0; i < clientList.length; i++) {
           const client = clientList[i]
-          if (client.url === 'http://localhost:3000/' && 'focus' in client)
+          if (
+            (client.url === 'https://pmdr.lucasalt.fr' && 'focus' in client) ||
+            (client.url === 'https://pmdr-dev.lucasalt.fr' &&
+              'focus' in client) ||
+            (client.url === 'http://localhost:3000/' && 'focus' in client)
+          )
             return client.focus()
         }
         if (clients.openWindow) return clients.openWindow('/')
